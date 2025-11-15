076a4586148ebfbd0d6a5b30928805ebc145bbf4
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-11-15 12:11:31 +0400

    feat: add main application server

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth";
+import eventRoutes from "./routes/events";
+import { errorHandler } from "./middlewares/error";
+import connectDB from "./config/db";
+import http from "http";
+import config from "./config/config";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+
+app.use(errorHandler);
+
+connectDB();
+
+// Start the server
+server.listen(config.port, () => {
+  console.log(
+    `Server running on port ${config.port} in ${config.environment} mode`
+  );
+});
